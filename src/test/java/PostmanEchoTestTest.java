80b22d6e2ed57f6f64860cfb89efbbd8765537e4
Maks-Bax <rn4aey@yandex.ru>
2022-08-31 15:41:05 +0300

    addOneTest

--- src/test/java/PostmanEchoTestTest.java
+++ src/test/java/PostmanEchoTestTest.java
@@ -16,4 +16,18 @@ public class PostmanEchoTestTest {
                 .body("data", equalTo("some data"))
         ;
     }
+
+    @Test
+    void shouldTakeRequest1() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет Мир")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет Мир"))
+        ;
+    }
 }
